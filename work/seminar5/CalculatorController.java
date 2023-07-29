413269742b599f678d8ba3b47089b1201dd01536
Sergey Antipov <ser_ok@mail.ru>
2023-07-29 11:41:19 +0300

    a: work.seminar5 // pattern MVP\MVC

---
+++ work/seminar5/CalculatorController.java
@@ -0,0 +1,31 @@
+package work.seminar5;
+
+public class CalculatorController {
+    private CalculatorModel model;
+    private CalculatorView view;
+
+    public CalculatorController(CalculatorModel model, CalculatorView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void performeOperation(String operation, int num1, int num2) {
+        switch (operation) {
+            case "+":
+                model.add(num1, num2);
+                break;
+            case "-":
+                model.subtract(num1, num2);
+                break;
+            case "*":
+                model.multiply(num1, num2);
+                break;
+            case "/":
+                model.divide(num1, num2);
+                break;
+            default:
+                System.out.println("Некорректная операция!");
+        }
+        view.displayResult(model.getResult());
+    }
+}
